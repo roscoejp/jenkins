0803f80507b4042ccaec091594014beccc00d69e
Roscoe Pyell <roscoejp@gmail.com>
2018-04-18 15:48:10 -0700

    Update simple-scripted.groovy

--- simple-scripted.groovy
+++ simple-scripted.groovy
@@ -1,15 +1,32 @@
 stage('Say Hello') { echo 'Hello' }
 
 parallel firstBranch: {
-    stage('Say World') { echo 'World' }
+    stage('Say World') { 
+        echo 'World' 
+    }
 }, secondBranch: {
-    stage('Fortune Cookie') { echo "${params.TEXTINPUT}" }
+    stage('Fortune Cookie') { 
+        echo "${params.TEXTINPUT}"
+        parallel firstBranch: {
+            stage('Say Nested Word') { 
+                echo 'Nested World' 
+            }
+        }, secondBranch: {
+            stage('Nested Fortune Cookie') { 
+                echo "Nested ${params.TEXTINPUT}" 
+            }
+        }
+    }
 }
 if (params.INPUT) {
-    stage('True') { echo 'I am True expression.' }
+    stage('True') { 
+        echo 'I am True expression.' 
+    }
 } 
 else {
-    stage('False') { echo 'I am a False expression.' }
+    stage('False') { 
+        echo 'I am a False expression.' 
+    }
 }
 
 //stage('Fail') { sh 'cp bob bob' }
